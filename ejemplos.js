1aa7edbcc3c98b294c6d25bb2dd69c3e9bf69809
ArgyStudio <mkt.avengers.make@gmail.com>
2025-08-15 14:36:28 -0300

    Update ejemplos.js

--- ejemplos.js
+++ ejemplos.js
@@ -2,21 +2,22 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 const ejemplos = [
-  'EJEMPLO: Cargás $14.000, Jugás con $84.000',
-  'EJEMPLO: Cargás $3.000, Jugás con $18.000',
-  'EJEMPLO: Cargás $9.000, Jugás con $54.000',
-  'EJEMPLO: Cargás $12.000, Jugás con $72.000',
-  'EJEMPLO: Cargás $5.000, Jugás con $30.000',
-  'EJEMPLO: Cargás $2.000, Jugás con $12.000',
-  'EJEMPLO: Cargás $7.000, Jugás con $42.000',
-  'EJEMPLO: Cargás $15.000, Jugás con $90.000',
-  'EJEMPLO: Cargás $10.000, Jugás con $60.000',
-  'EJEMPLO: Cargás $6.000, Jugás con $36.000',
-  'EJEMPLO: Cargás $13.000, Jugás con $78.000',
-  'EJEMPLO: Cargás $8.000, Jugás con $48.000',
-  'EJEMPLO: Cargás $4.000, Jugás con $24.000',
-  'EJEMPLO: Cargás $11.000, Jugás con $66.000'
+  'EJEMPLO: Cargás $14.000, Jugás con $56.000',
+  'EJEMPLO: Cargás $3.000, Jugás con $12.000',
+  'EJEMPLO: Cargás $9.000, Jugás con $36.000',
+  'EJEMPLO: Cargás $12.000, Jugás con $48.000',
+  'EJEMPLO: Cargás $5.000, Jugás con $20.000',
+  'EJEMPLO: Cargás $2.000, Jugás con $8.000',
+  'EJEMPLO: Cargás $7.000, Jugás con $28.000',
+  'EJEMPLO: Cargás $15.000, Jugás con $60.000',
+  'EJEMPLO: Cargás $10.000, Jugás con $40.000',
+  'EJEMPLO: Cargás $6.000, Jugás con $24.000',
+  'EJEMPLO: Cargás $13.000, Jugás con $52.000',
+  'EJEMPLO: Cargás $8.000, Jugás con $32.000',
+  'EJEMPLO: Cargás $4.000, Jugás con $16.000',
+  'EJEMPLO: Cargás $11.000, Jugás con $44.000'
 ];
+
 
 
 
@@ -33,3 +34,4 @@ const ejemplos = [
   nextEjemplo();
   setInterval(nextEjemplo, intervaloMs);
 });
+
